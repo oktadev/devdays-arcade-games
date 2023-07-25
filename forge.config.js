2d9befdb13e6b7f87e9d3769ce1d4f5dc46f79ea
Deepu <d4udts@gmail.com>
2023-07-25 18:31:00 +0200

    setup ci/cd

--- forge.config.js
+++ forge.config.js
@@ -5,21 +5,13 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
     },
     {
       name: '@electron-forge/maker-deb',
       config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    }
   ],
   plugins: [
     {
